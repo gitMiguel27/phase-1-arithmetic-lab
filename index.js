328212a114ee1de46f869a9c829984669b5349cd
Miguel Nazario <mnazario35@gmail.com>
2022-01-30 11:35:47 -0500

    commit tree 59b27f70059c432e2a42370d6f2d13bafad9e173

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10) + 1;
+
+const num3 = 26;
+const num4 = 11;
+
+let mod = num3 % num4;
+
+const set = (3, 4, 20);
+let max =  Math.max(set);
